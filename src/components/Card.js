a43b0b4a37935e6488df5a5c24207e94728b0374
oona <risseado@gene.com>
2022-04-22 13:59:57 -0700

    deleted lines

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,16 +2,9 @@ import React from "react"
 
 
 export default function Card(props) {
-    // let badgeText
-    // if (props.info.openSpots === 0) {
-    //     badgeText = "SOLD OUT"
-    // } else if (props.info.location === "Online") {
-    //     badgeText = "ONLINE"
-    // }
-    // console.log(props.info.coverImg)
+
     return (
         <div className="card">
-            {/* {badgeText && <div className="card--badge">{badgeText}</div>} */}
             <img className="card--image" src={process.env.PUBLIC_URL+"/images/"+props.info.coverImg}/>
             <div className="card--stats">
                 <img className ="card--star" src={process.env.PUBLIC_URL+"/images/star.png"}/>
